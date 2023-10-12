ebd020f7aca1d838847e9fc54ed01d0313ff252d
swayamps <109921815+swayamps@users.noreply.github.com>
2023-10-12 20:27:55 +0530

    using streamlit instead of matplotlib in analysis

--- pages/analysisofgenre.py
+++ pages/analysisofgenre.py
@@ -1,10 +1,49 @@
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import PIL
+# from PIL import Image
+# import streamlit as st
+# from st_pages import add_page_title
+# add_page_title(layout="narrow")
+
+# st.markdown("""
+#     ## Analysis of Genre
+    
+#     Below is the Pie-Chart of the Genres of Music featured in BillBoard during 1999-2019
+    
+#     """)
+
+# # Load and preprocess the dataset
+# df = pd.read_csv("billboard.csv")
+# df['Week'] = pd.to_datetime(df['Week'], format='%d-%m-%Y')
+# df['Genres'] = df['Genre'].str.split(',')
+# df = df.explode('Genre')
+
+# # Flatten the list of genres
+# genres_list = [genre for genres in df['Genres'] for genre in genres]
+
+# # Calculate the frequency of each genre
+# genre_counts = pd.Series(genres_list).value_counts()
+
+# # Select top 10 genres and group the rest as "Others"
+# top_genres = genre_counts.head(6)
+# other_count = genre_counts[6:].sum()
+# top_genres['Others'] = other_count
+
+
+# # Plot the genre distribution in a pie chart
+# plt.figure(figsize=(12, 6))
+# plt.pie(top_genres, labels=top_genres.index, autopct='%1.1f%%', startangle=90)
+# plt.axis('equal')
+# plt.title('Genre Distribution')
+# total_genres = len(genre_counts)
+# plt.text(0, -1.3, f"Total Genres: {total_genres}", horizontalalignment='center', verticalalignment='center')
+# st.pyplot(plt.gcf())
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
-import PIL
-from PIL import Image
 import streamlit as st
-from st_pages import add_page_title
-add_page_title(layout="narrow")
 
 st.markdown("""
     ## Analysis of Genre
@@ -30,12 +69,13 @@ top_genres = genre_counts.head(6)
 other_count = genre_counts[6:].sum()
 top_genres['Others'] = other_count
 
-
-# Plot the genre distribution in a pie chart
-plt.figure(figsize=(12, 6))
-plt.pie(top_genres, labels=top_genres.index, autopct='%1.1f%%', startangle=90)
-plt.axis('equal')
-plt.title('Genre Distribution')
+# Plot the genre distribution in a pie chart using Streamlit
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.pie(top_genres, labels=top_genres.index, autopct='%1.1f%%', startangle=90)
+ax.axis('equal')
+ax.set_title('Genre Distribution')
 total_genres = len(genre_counts)
-plt.text(0, -1.3, f"Total Genres: {total_genres}", horizontalalignment='center', verticalalignment='center')
-st.pyplot(plt.gcf())
+ax.text(0, -1.3, f"Total Genres: {total_genres}", horizontalalignment='center', verticalalignment='center')
+
+# Display the chart using Streamlit
+st.pyplot(fig)
