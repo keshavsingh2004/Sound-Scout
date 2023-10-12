e680860b00bf0afa338d3bf39f130eab0cd9d52f
swayamps <109921815+swayamps@users.noreply.github.com>
2023-10-12 20:55:09 +0530

    st.echo

--- pages/app.py
+++ pages/app.py
@@ -1,14 +1,18 @@
-from st_pages import Page, Section, add_page_title, show_pages
 from pathlib import Path
-## Declaring the pages in your app:
-show_pages(
-    [
-        Page("pages/home.py", "Home"),
-        Section(name="Analysis of Artists"),
-        Page("pages/top_5.py", "Top 5"),
-        Page("pages/comparison.py", "Comparison"),
-        Page("pages/analysisofgenre.py","Analysis of Genre",in_section=False),
-        Page("pages/linear","LinearRegression")
-    ]
-)
-add_page_title()
+import streamlit as st
+with st.echo("below"):
+    from st_pages import Page, Section, add_page_title, show_pages
+
+    ## Declaring the pages in your app:
+
+    show_pages(
+        [
+                Page("pages/home.py", "Home"),
+            Section(name="Analysis of Artists"),
+            Page("pages/top_5.py", "Top 5"),
+            Page("pages/comparison.py", "Comparison"),
+            Page("pages/analysisofgenre.py","Analysis of Genre",in_section=False),
+            Page("pages/linear","LinearRegression")
+        ]
+    )
+    add_page_title()
