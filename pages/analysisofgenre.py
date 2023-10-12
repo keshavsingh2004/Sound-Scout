dc63565bcf6f06946b0fb09063c7843a8a2688ae
swayamps <109921815+swayamps@users.noreply.github.com>
2023-10-12 19:45:02 +0530

    renaming csv file

--- pages/analysisofgenre.py
+++ pages/analysisofgenre.py
@@ -14,7 +14,7 @@ st.markdown("""
     """)
 
 # Load and preprocess the dataset
-df = pd.read_csv("billboardHot100_1999-2019.csv")
+df = pd.read_csv("billboard.csv")
 df['Week'] = pd.to_datetime(df['Week'], format='%d-%m-%Y')
 df['Genres'] = df['Genre'].str.split(',')
 df = df.explode('Genre')
