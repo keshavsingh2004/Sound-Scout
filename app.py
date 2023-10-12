1c8ebd2536e7c630c42d30186d531f7b7cbc3dc4
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-12 15:27:56 +0530

    page

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from st_pages import Page, add_page_title, show_pages
-
+from pathlib import Path
 ## Declaring the pages in your app:
 show_pages(
     [
-        Page("home.py", "Home", "🏠"),
+        Page("keshavsingh2004/music-artists-trends/home.py", "Home", "🏠"),
         Page("top_5.py", "Top 5", "🏆"),
         Page("comparison.py", "Comparison", "⚖️"),
     ]
