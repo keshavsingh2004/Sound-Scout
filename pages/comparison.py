6dff5babfd9290215785adbb2d5248be16d4736f
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-12 15:43:23 +0530

    page

--- pages/comparison.py
+++ pages/comparison.py
@@ -3,6 +3,22 @@ from PIL import Image
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+df = pd.read_csv("charts.csv")
+
+# Convert the 'Week' column to datetime format
+df['Year'] = pd.to_datetime(df['Week'], format='%d-%m-%Y')
+
+# Calculate the frequency of each artist
+artist_counts = df['Artists'].value_counts()
+
+# Get the top 5 artists from user input
+top_5_artists = ['Taylor Swift', 'Elton John', 'Madonna', 'Drake', 'Kenny Chesney']
+
+# Filter the dataset for the top 5 artists
+top_5_artists_data = df[df['Artists'].isin(top_5_artists)]
+
+# Group and aggregate data at the yearly level for the top 5 artists
+grouped = top_5_artists_data.groupby(['Year', 'Artists']).size().reset_index(name='Count')
 st.header("Comparison")
 plt.figure(figsize=(10, 6))
 for artist in top_5_artists:
