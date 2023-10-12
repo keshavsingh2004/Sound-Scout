13ce4e40580dad15ec0eff227b62d426f6e55649
swayamps <109921815+swayamps@users.noreply.github.com>
2023-10-12 20:50:42 +0530

    in_section

--- pages/app.py
+++ pages/app.py
@@ -7,7 +7,7 @@ show_pages(
         Section(name="Analysis of Artists"),
         Page("pages/top_5.py", "Top 5"),
         Page("pages/comparison.py", "Comparison"),
-        Page("pages/analysisofgenre.py","Analysis of Genre"),
+        Page("pages/analysisofgenre.py","Analysis of Genre",in_section=False),
         Page("pages/linear","LinearRegression")
     ]
 )
