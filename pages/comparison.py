c916d41e73705c52f863bb1bb55c60ba729de292
swayamps <109921815+swayamps@users.noreply.github.com>
2023-10-13 20:20:09 +0530

    comparison

--- pages/comparison.py
+++ pages/comparison.py
@@ -1,3 +1,32 @@
+# import streamlit as st
+# import pandas as pd
+# import plotly.express as px
+
+# df = pd.read_csv("charts.csv")
+
+# # Convert the 'Week' column to datetime format
+# df['Year'] = pd.to_datetime(df['Week'], format='%d-%m-%Y')
+
+# # Calculate the frequency of each artist
+# artist_counts = df['Artists'].value_counts()
+
+# # Get the top 5 artists from user input
+# top_5_artists = ['Taylor Swift', 'Elton John', 'Madonna', 'Drake', 'Kenny Chesney']
+
+# # Filter the dataset for the top 5 artists
+# top_5_artists_data = df[df['Artists'].isin(top_5_artists)]
+
+# # Group and aggregate data at the yearly level for the top 5 artists
+# grouped = top_5_artists_data.groupby(['Year', 'Artists']).size().reset_index(name='Count')
+
+# st.header("Comparison")
+
+# # Create the Plotly line chart for the top 5 artists
+# chart = px.line(grouped, x='Year', y='Count', color='Artists', title='Artist Count Over the Years - Top 5 Artists (User Provided)')
+
+# # Display the chart using Streamlit
+# st.plotly_chart(chart, theme=None, use_container_width=True)
+
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -7,22 +36,21 @@ df = pd.read_csv("charts.csv")
 # Convert the 'Week' column to datetime format
 df['Year'] = pd.to_datetime(df['Week'], format='%d-%m-%Y')
 
-# Calculate the frequency of each artist
-artist_counts = df['Artists'].value_counts()
+# Ask the user for two artists
+artist1 = st.text_input("Enter the first artist:")
+artist2 = st.text_input("Enter the second artist:")
 
-# Get the top 5 artists from user input
-top_5_artists = ['Taylor Swift', 'Elton John', 'Madonna', 'Drake', 'Kenny Chesney']
+# Filter the dataset for the user-provided artists
+artists_data = df[df['Artists'].isin([artist1, artist2])]
 
-# Filter the dataset for the top 5 artists
-top_5_artists_data = df[df['Artists'].isin(top_5_artists)]
-
-# Group and aggregate data at the yearly level for the top 5 artists
-grouped = top_5_artists_data.groupby(['Year', 'Artists']).size().reset_index(name='Count')
+# Group and aggregate data at the yearly level for the user-provided artists
+grouped = artists_data.groupby(['Year', 'Artists']).size().reset_index(name='Count')
 
 st.header("Comparison")
 
-# Create the Plotly line chart for the top 5 artists
-chart = px.line(grouped, x='Year', y='Count', color='Artists', title='Artist Count Over the Years - Top 5 Artists (User Provided)')
+# Create the Plotly line chart for the user-provided artists
+chart = px.line(grouped, x='Year', y='Count', color='Artists',
+                title=f"Artist Count Over the Years - {artist1} vs {artist2}")
 
 # Display the chart using Streamlit
-st.plotly_chart(chart, theme=None, use_container_width=True)
+st.plotly_chart(chart, use_container_width=True)
