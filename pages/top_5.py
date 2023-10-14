7a58bea28d4d03a56e3684045f23b27bd55101e0
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-15 01:04:18 +0530

    wiki

--- pages/top_5.py
+++ pages/top_5.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import requests
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 # Wikipedia API endpoint
 WIKIPEDIA_API_URL = 'https://en.wikipedia.org/w/api.php'
@@ -9,7 +10,9 @@ WIKIPEDIA_API_URL = 'https://en.wikipedia.org/w/api.php'
 # Load data
 df = pd.read_csv("charts.csv")
 
-st.title("Artist Analysis")
+# Load the BART model
+bart_model = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-large-cnn")
+bart_tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
 
 def get_artist_info(artist_name):
   params = {
@@ -22,10 +25,19 @@ def get_artist_info(artist_name):
   }
   response = requests.get(WIKIPEDIA_API_URL, params=params).json()
   pages = response.get('query', {}).get('pages', {})
-  page = next(iter(pages.values()))  # Get the first page
+  page = next(iter(pages.values()))
   description = page.get('extract', '')
 
   return description
+
+def summarize_description(description):
+  """Summarizes the given description using the BART model."""
+
+  input_ids = bart_tokenizer(description, return_tensors="pt").input_ids
+  outputs = bart_model.generate(input_ids, max_length=100, early_stopping=True)
+  summary = bart_tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+  return summary
 
 # Convert the 'Week' column to datetime format
 df['Year'] = pd.to_datetime(df['Week'], format='%d-%m-%Y')
@@ -56,18 +68,28 @@ if analysis_option == "Artist Discography over Time":
   # Display the image and about section for the selected artist
   if selected_artist in top_5_artists:
     description = get_artist_info(selected_artist)
+    summary = summarize_description(description)
+
+    # Display the image
+    st.image(f"https://en.wikipedia.org/wiki/{selected_artist}")
+
+    # Display the description and summary
     st.markdown(f'## About {selected_artist}')
     st.markdown(description)
+    st.markdown(f'**Summary:** {summary}')
+
     st.plotly_chart(fig)
 
 elif analysis_option == "Artist Comparison":
   st.subheader("Artist Comparison")
+  
   # Get the unique list of artists
   unique_artists = df['Artists'].unique().tolist()
 
   # Ask the user to select artists using multiselect dropdown
   selected_artists = st.multiselect("Select artists:", unique_artists)
 
+  
   if len(selected_artists) > 0:
     # Filter the dataset for the selected artists
     artists_data = df[df['Artists'].isin(selected_artists)]
@@ -78,8 +100,8 @@ elif analysis_option == "Artist Comparison":
     st.header("Comparison")
 
     # Create the Plotly line chart for the selected artists
-    chart = px.line(grouped, x='Year', y='Count', color='Artists',
-                    title="Artist Count Over the Years - Comparison")
-
-    # Display the chart using Streamlit
-    st.plotly_chart(chart)
+    fig = px.line(grouped, x='Year', y='Count', color='Artists', title='Artist Comparison Over the Years')
+    st.plotly_chart(fig)
+    
+else:
+   st.write("Please select at least one artist.")
