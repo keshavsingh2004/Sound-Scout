e07bb86de25d815d5b6476f62d9f7e67bddde2f4
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-15 00:35:43 +0530

    wiki

--- pages/top_5.py
+++ pages/top_5.py
@@ -1,61 +1,65 @@
 import PIL
 from PIL import Image
 import streamlit as st
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
 import requests
 import pandas as pd
-import plotly.express as px
 
-# Spotify API credentials
-CLIENT_ID = 'd55c490e4f9c4372ac59952d422fe1fd'
-CLIENT_SECRET = 'ca902e2a8d7b43ad8cb3a0ed682bbff8'
-
-# Authenticate with the Spotify API
-auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-sp = spotipy.Spotify(auth_manager=auth_manager)
+# Wikipedia API endpoint
+WIKIPEDIA_API_URL = 'https://en.wikipedia.org/w/api.php'
 
 # Load data
 df = pd.read_csv("charts.csv")
 
 st.title("Artist Analysis")
 
+# Function to retrieve artist image URL from Wikipedia API
+def get_artist_image_url(artist_name):
+    params = {
+        'action': 'query',
+        'format': 'json',
+        'prop': 'pageimages',
+        'piprop': 'original',
+        'titles': artist_name
+    }
+    response = requests.get(WIKIPEDIA_API_URL, params=params).json()
+    pages = response.get('query', {}).get('pages', {})
+    page = next(iter(pages.values()))  # Get the first page
+    image_url = page.get('original', {}).get('source', '')
+
+    return image_url
+
+# Function to retrieve artist description from Wikipedia API
+def get_artist_description(artist_name):
+    params = {
+        'action': 'query',
+        'format': 'json',
+        'prop': 'extracts',
+        'exintro': True,
+        'explaintext': True,
+        'titles': artist_name
+    }
+    response = requests.get(WIKIPEDIA_API_URL, params=params).json()
+    pages = response.get('query', {}).get('pages', {})
+    page = next(iter(pages.values()))  # Get the first page
+    description = page.get('extract', '')
+
+    return description
+
 def get_artist_info(artist_name):
-    # Search for the artist
-    results = sp.search(q=artist_name, type='artist', limit=1)
-
-    if results['artists']['items']:
-        artist = results['artists']['items'][0]
-
-        # Get the artist's images
-        images = artist['images']
-        if images:
-            image_url = images[0]['url']
-            st.image(image_url, caption=artist_name)
-
-        # Get the artist's description
-        artist_id = artist['id']
-        headers = {
-            'Authorization': f'Bearer {sp.auth_manager.get_access_token()}'
-        }
-        url = f'https://api.spotify.com/v1/artists/{artist_id}'
-        response = requests.get(url, headers=headers)
-
-        if response.status_code == 200:
-            artist_info = response.json()
-            description = artist_info.get('bio', {}).get('summary')
-            if description:
-                st.markdown(f'## About {artist_name}')
-                st.markdown(description)
-            else:
-                st.write(f"No description available for {artist_name}.")
-        else:
-            st.write(f"Error retrieving artist information for {artist_name}.")
+    # Get the artist's image URL from Wikipedia
+    image_url = get_artist_image_url(artist_name)
+    if image_url:
+        st.image(image_url, caption=artist_name)
     else:
-        st.write(f"No artist found with the name {artist_name}.")
+        st.write(f"No image available for {artist_name}.")
 
-# Convert the 'Week' column to datetime format
-df['Year'] = pd.to_datetime(df['Week'], format='%d-%m-%Y')
+    # Get the artist's description from Wikipedia
+    description = get_artist_description(artist_name)
+    if description:
+        st.markdown(f'## About {artist_name}')
+        st.markdown(description)
+    else:
+        st.write(f"No description available for {artist_name}.")
 
 analysis_option = st.radio("Choose an analysis option:", ("Artist Discography over Time", "Artist Comparison"))
 
@@ -65,7 +69,7 @@ if analysis_option == "Artist Discography over Time":
     artist_counts = df['Artists'].value_counts()
 
     # Get the top 5 artists from user input
-    top_5_artists = ['Taylor Swift', 'Elton John', 'Madonna', 'Drake', 'Kenny Chesney','Travis Scott','Ed Sheeran','Doja Cat','Kendrick Lamar','Justin Bieber', 'One Direction', 'Zayn','Harry Styles','Niall Horan', 'The Weeknd']
+    top_5_artists = ['Taylor Swift', 'Elton John', 'Madonna', 'Drake', 'Kenny Chesney']
 
     # Filter the dataset for the top 5 artists
     top_5_artists_data = df[df['Artists'].isin(top_5_artists)]
@@ -76,13 +80,11 @@ if analysis_option == "Artist Discography over Time":
 
     # Plot the graph for the selected artist
     chart_data = grouped[grouped['Artists'] == selected_artist]
-    fig = px.line(chart_data, x='Year', y='Count', title=f'Artist Count Over the Years - {selected_artist}')
-    fig.update_traces(line=dict(color='green'))
+    st.plotly_chart(chart_data)
 
     # Display the image and about us section for the selected artist
     if selected_artist in top_5_artists:
         get_artist_info(selected_artist)
-        st.plotly_chart(fig)
 
 elif analysis_option == "Artist Comparison":
     st.subheader("Artist Comparison")
@@ -93,17 +95,17 @@ elif analysis_option == "Artist Comparison":
     selected_artists = st.multiselect("Select artists:", unique_artists)
 
     if len(selected_artists) > 0:
-    # Filter the dataset for the selected artists
+        # Filter the dataset for the selected artists
         artists_data = df[df['Artists'].isin(selected_artists)]
 
-    # Group and aggregate data at the yearly level for the selected artists
+        # Group and aggregate data at the yearly level for the selected artists
         grouped = artists_data.groupby(['Year', 'Artists']).size().reset_index(name='Count')
 
         st.header("Comparison")
 
-    # Create the Plotly line chart for the selected artists
+        # Create the Plotly line chart for the selected artists
         chart = px.line(grouped, x='Year', y='Count', color='Artists',
                         title="Artist Count Over the Years - Comparison")
 
-    # Display the chart using Streamlit
+        # Display the chart using Streamlit
         st.plotly_chart(chart)
