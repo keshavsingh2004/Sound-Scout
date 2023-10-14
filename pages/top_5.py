e2cbfbd2a0ab2022d2b6dcf951d5cb0ca34f56d7
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-15 00:46:34 +0530

    wiki

--- pages/top_5.py
+++ pages/top_5.py
@@ -3,61 +3,38 @@ from PIL import Image
 import streamlit as st
 import requests
 import pandas as pd
-
-# Wikipedia API endpoint
-WIKIPEDIA_API_URL = 'https://en.wikipedia.org/w/api.php'
+import plotly.express as px
+import wikipedia
 
 # Load data
 df = pd.read_csv("charts.csv")
 
 st.title("Artist Analysis")
 
-def get_artist_image_url(artist_name):
-    params = {
-        'action': 'query',
-        'format': 'json',
-        'prop': 'pageimages',
-        'piprop': 'original',
-        'titles': artist_name
-    }
-    response = requests.get(WIKIPEDIA_API_URL, params=params).json()
-    pages = response.get('query', {}).get('pages', {})
-    page = next(iter(pages.values()))  # Get the first page
-    image_url = page.get('original', {}).get('source', '')
-
-    return image_url
-
-def get_artist_description(artist_name):
-    params = {
-        'action': 'query',
-        'format': 'json',
-        'prop': 'extracts',
-        'exintro': True,
-        'explaintext': True,
-        'titles': artist_name
-    }
-    response = requests.get(WIKIPEDIA_API_URL, params=params).json()
-    pages = response.get('query', {}).get('pages', {})
-    page = next(iter(pages.values()))  # Get the first page
-    description = page.get('extract', '')
-
-    return description
-
 def get_artist_info(artist_name):
-    # Get the artist's image URL from Wikipedia
-    image_url = get_artist_image_url(artist_name)
-    if image_url:
-        st.image(image_url, caption=artist_name)
-    else:
-        st.write(f"No image available for {artist_name}.")
+    try:
+        # Search for the artist's page on Wikipedia
+        page = wikipedia.page(artist_name)
 
-    # Get the artist's description from Wikipedia
-    description = get_artist_description(artist_name)
-    if description:
+        # Get the artist's summary and image
+        summary = page.summary
+        image_url = page.images[0]
+
+        # Display the artist's image
+        image = Image.open(requests.get(image_url, stream=True).raw)
+        st.image(image, caption=artist_name)
+
+        # Display the artist's summary
         st.markdown(f'## About {artist_name}')
-        st.markdown(description)
-    else:
-        st.write(f"No description available for {artist_name}.")
+        st.markdown(summary)
+
+    except wikipedia.exceptions.PageError:
+        st.write(f"No Wikipedia page found for {artist_name}.")
+    except wikipedia.exceptions.DisambiguationError:
+        st.write(f"Multiple Wikipedia pages found for {artist_name}. Please specify the artist more precisely.")
+
+# Convert the 'Week' column to datetime format
+df['Year'] = pd.to_datetime(df['Week'], format='%d-%m-%Y')
 
 analysis_option = st.radio("Choose an analysis option:", ("Artist Discography over Time", "Artist Comparison"))
 
@@ -66,23 +43,26 @@ if analysis_option == "Artist Discography over Time":
     # Calculate the frequency of each artist
     artist_counts = df['Artists'].value_counts()
 
-    # Get the top 5 artists from user input
-    top_5_artists = ['Taylor Swift', 'Elton John', 'Madonna', 'Drake', 'Kenny Chesney']
+    # Get the top 5 artists
+    top_5_artists = artist_counts.head(5).index.tolist()
 
     # Filter the dataset for the top 5 artists
     top_5_artists_data = df[df['Artists'].isin(top_5_artists)]
 
     # Group and aggregate data at the yearly level for the top 5 artists
     grouped = top_5_artists_data.groupby(['Year', 'Artists']).size().reset_index(name='Count')
-    selected_artist = st.selectbox("Select an artist:", [artist for artist in top_5_artists], index=0)
+
+    selected_artist = st.selectbox("Select an artist:", top_5_artists)
 
     # Plot the graph for the selected artist
     chart_data = grouped[grouped['Artists'] == selected_artist]
-    st.plotly_chart(chart_data)
+    fig = px.line(chart_data, x='Year', y='Count', title=f'Artist Count Over the Years - {selected_artist}')
+    fig.update_traces(line=dict(color='green'))
 
-    # Display the image and about us section for the selected artist
+    # Display the image and about section for the selected artist
     if selected_artist in top_5_artists:
         get_artist_info(selected_artist)
+        st.plotly_chart(fig)
 
 elif analysis_option == "Artist Comparison":
     st.subheader("Artist Comparison")
