4b6dbd5ae656202f4931d39e0b755eeedbac239f
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-14 18:05:59 +0530

    graph

--- pages/polarplot.py
+++ pages/polarplot.py
@@ -3,46 +3,37 @@ import plotly.graph_objects as go
 import streamlit as st
 
 def feature_plot(features):
-
     labels = features.columns.tolist()
     stats = features.mean().tolist()
 
     angles = np.linspace(0, 2*np.pi, len(labels), endpoint=False)
 
-    # close the plot
-    stats = np.concatenate((stats,[stats[0]]))
-    angles = np.concatenate((angles,[angles[0]]))
+    # Close the plot
+    stats = np.concatenate((stats, [stats[0]]))
+    angles = np.concatenate((angles, [angles[0]]))
 
-    # Create a Plotly figure
-    fig = go.Figure()
-
-    # Add a polar trace to the figure
-    fig.add_trace(go.Scatter(
-        theta=angles,
+    fig = go.Figure(data=go.Scatterpolar(
         r=stats,
-        mode='lines+markers',
-        line_width=2,
-        name="Features",
+        theta=angles,
         fill='toself',
-        fill_color='gray',
+        name='Features',
+        line_color='gray',
         opacity=0.75
     ))
 
-    # Set the layout of the figure
     fig.update_layout(
         polar=dict(
-            angularaxis=dict(
-                ticks='outside',
-                labels=labels
-            )
-          ),
-        radialaxis=dict(
-            tickmode='array',
-            tickvals=[0, 0.2, 0.4, 0.6, 0.8, 1],
-            ticktext=["0", "0.2", "0.4", "0.6", "0.8", "1.0"]
-          ),
-        legend=dict(x=0.1, y=0.1)
+            radialaxis=dict(
+                visible=True,
+                range=[0, 1],
+                tickvals=[0.2, 0.4, 0.6, 0.8],
+                ticktext=["0.2", '0.4', "0.6", "0.8"],
+                color="grey",
+                tickfont=dict(size=12),
+            ),
+        ),
+        showlegend=True,
+        legend=dict(x=0.1, y=0.1),
     )
 
-    # Display the figure in Streamlit
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, use_container_width=True)
