5381d3bce95ba7faf935618c41225fbe41bfb65d
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-15 01:44:52 +0530

    no summary

--- pages/top_5.py
+++ pages/top_5.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import requests
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 # Wikipedia API endpoint
 WIKIPEDIA_API_URL = 'https://en.wikipedia.org/w/api.php'
@@ -10,9 +9,6 @@ WIKIPEDIA_API_URL = 'https://en.wikipedia.org/w/api.php'
 # Load data
 df = pd.read_csv("charts.csv")
 
-# Load the BART model
-bart_model = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-large-cnn")
-bart_tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
 
 def get_artist_info(artist_name):
   params = {
@@ -29,15 +25,6 @@ def get_artist_info(artist_name):
   description = page.get('extract', '')
 
   return description
-
-def summarize_description(description):
-  """Summarizes the given description using the BART model."""
-
-  input_ids = bart_tokenizer(description, return_tensors="pt").input_ids
-  outputs = bart_model.generate(input_ids, max_length=100, early_stopping=True)
-  summary = bart_tokenizer.decode(outputs[0], skip_special_tokens=True)
-
-  return summary
 
 # Convert the 'Week' column to datetime format
 df['Year'] = pd.to_datetime(df['Week'], format='%d-%m-%Y')
@@ -68,16 +55,13 @@ if analysis_option == "Artist Discography over Time":
   # Display the image and about section for the selected artist
   if selected_artist in top_5_artists:
     description = get_artist_info(selected_artist)
-    summary = summarize_description(description)
-
+  
     # Display the image
     st.image(f"https://en.wikipedia.org/wiki/{selected_artist}")
 
     # Display the description and summary
     st.markdown(f'## About {selected_artist}')
     st.markdown(description)
-    st.markdown(f'**Summary:** {summary}')
-
     st.plotly_chart(fig)
 
 elif analysis_option == "Artist Comparison":
