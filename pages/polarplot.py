8f226baf3a3382d45d3663a31f0c15fd94a5a06e
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-14 18:12:39 +0530

    graph

--- pages/polarplot.py
+++ pages/polarplot.py
@@ -1,39 +1,46 @@
 import numpy as np
 import plotly.graph_objects as go
-import streamlit as st
 
 def feature_plot(features):
+
     labels = features.columns.tolist()
     stats = features.mean().tolist()
 
     angles = np.linspace(0, 2*np.pi, len(labels), endpoint=False)
 
-    # Close the plot
-    stats = np.concatenate((stats, [stats[0]]))
-    angles = np.concatenate((angles, [angles[0]]))
+    # close the plot
+    stats = np.concatenate((stats,[stats[0]]))
+    angles = np.concatenate((angles,[angles[0]]))
 
-    fig = go.Figure(data=go.Scatterpolar(
-        r=stats,
+    # Create a Plotly figure
+    fig = go.Figure()
+
+    # Add a polar trace to the figure
+    fig.add_trace(go.Scatter(
         theta=angles,
+        r=stats,
+        mode='lines+markers',
+        line_width=2,
+        name="Features",
         fill='toself',
-        name='Features',
-        line_color='gray',
+        fill_color='gray',
         opacity=0.75
     ))
 
+    # Set the layout of the figure
     fig.update_layout(
         polar=dict(
-            radialaxis=dict(
-                visible=True,
-                range=[0, 1],
-                tickvals=[0.2, 0.4, 0.6, 0.8],
-                ticktext=["0.2", '0.4', "0.6", "0.8"],
-                color="grey",
-                tickfont=dict(size=12),
-            ),
-        ),
-        showlegend=True,
-        legend=dict(x=0.1, y=0.1),
+            angularaxis=dict(
+                ticks='outside',
+                labels=labels
+            )
+          ),
+        radialaxis=dict(
+            tickmode='array',
+            tickvals=[0, 0.2, 0.4, 0.6, 0.8, 1],
+            ticktext=["0", "0.2", "0.4", "0.6", "0.8", "1.0"]
+          ),
+        legend=dict(x=0.1, y=0.1)
     )
 
-    st.plotly_chart(fig, use_container_width=True)
+    return fig
