132e5ccdea45eff9e2d6833d92f27560a129e4cd
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-14 18:19:53 +0530

    back

--- pages/polarplot.py
+++ pages/polarplot.py
@@ -1,51 +1,30 @@
 import numpy as np
-import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 import streamlit as st
 
 def feature_plot(features):
+
     labels = features.columns.tolist()
     stats = features.mean().tolist()
 
-    angles = np.linspace(0, 2 * np.pi, len(labels), endpoint=False)
+    angles = np.linspace(0, 2*np.pi, len(labels), endpoint=False)
 
-    # Close the plot
-    stats = np.concatenate((stats, [stats[0]]))
-    angles = np.concatenate((angles, [angles[0]]))
+    # close the plot
+    stats = np.concatenate((stats,[stats[0]]))
+    angles = np.concatenate((angles,[angles[0]]))
 
-    fig = go.Figure()
+    # Size of the figure
+    fig = plt.figure(figsize=(18, 18))
 
-    fig.add_trace(go.Scatterpolar(
-        r=stats,
-        theta=angles,
-        fill='toself',
-        name='Features',
-        line_color='gray',
-        opacity=0.75
-    ))
+    ax = fig.add_subplot(221, polar=True)
+    ax.plot(angles, stats, 'o-', linewidth=2, label="Features", color='gray', alpha=0.75)
+    ax.fill(angles, stats, alpha=0.5, facecolor='gray')
+    ax.set_thetagrids(angles[0:7] * 180/np.pi, labels, fontsize=13)
 
-    fig.update_layout(
-        polar=dict(
-            radialaxis=dict(
-                visible=True,
-                range=[0, 1],
-                tickvals=[0.2, 0.4, 0.6, 0.8],
-                ticktext=["0.2", '0.4', "0.6", "0.8"],
-                color="grey",
-                tickfont=dict(size=12),
-                angle=0,  # Start angle at 0 degrees
-                dtick=0.2,  # Distance between ticks
-            ),
-            angularaxis=dict(
-                tickfont=dict(size=12),
-                rotation=90,  # Rotate the angle labels
-            )
-        ),
-        showlegend=True,
-        legend=dict(x=0.1, y=0.1),
-        width=600,
-        height=600,
-        margin=dict(l=50, r=50, t=50, b=50),
-        template='plotly_white',  # Use a white background template
-    )
+    ax.set_rlabel_position(250)
+    plt.yticks([0.2, 0.4, 0.6, 0.8], ["0.2", '0.4', "0.6", "0.8"], color="grey", size=12)
+    plt.ylim(0, 1)
 
-    st.plotly_chart(fig, use_container_width=True)
+    plt.legend(loc='best', bbox_to_anchor=(0.1, 0.1))
+
+    st.pyplot(fig, bbox_inches='tight')
