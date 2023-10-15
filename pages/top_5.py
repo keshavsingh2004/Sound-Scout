def35087693970a7438b1079f3ac55f5dd0f67b9
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-15 16:21:45 +0530

    moderate

--- pages/top_5.py
+++ pages/top_5.py
@@ -9,7 +9,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import wikipedia
 from st_pages import add_page_title
 
-add_page_title(layout="narrow")
+add_page_title(layout="moderate")
 
 # Spotify API credentials
 CLIENT_ID = 'd55c490e4f9c4372ac59952d422fe1fd'
