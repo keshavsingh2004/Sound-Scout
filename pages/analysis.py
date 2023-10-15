b04aa1f9f8fb59e6be98e64b368e57d66a43f2f8
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-15 17:10:18 +0530

    add song

--- pages/analysis.py
+++ pages/analysis.py
@@ -15,45 +15,58 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 
 st.header('Analysis of songs')
 
-search_selected = 'Artist'
+search_selected = st.selectbox("Search by:", ["Artist", "Song"])
 
-search_keyword = st.text_input(search_selected + " (Keyword Search)")
+if search_selected == 'Artist':
+    search_keyword = st.text_input("Artist (Keyword Search)")
+elif search_selected == 'Song':
+    search_keyword = st.text_input("Song (Keyword Search)")
+
 button_clicked = st.button("Search")
 
 search_results = []
 
-if search_keyword is not None and len(str(search_keyword)) > 0:
-    st.write("Start artist search")
-    artists = sp.search(q='artist:' + search_keyword, type='artist', limit=20)
-    artists_list = artists['artists']['items']
-    
-    if len(artists_list) > 0:
-        for artist in artists_list:
-            search_results.append(artist['name'])
+if button_clicked and search_keyword is not None and len(str(search_keyword)) > 0:
+    if search_selected == 'Artist':
+        st.write("Start artist search")
+        artists = sp.search(q='artist:' + search_keyword, type='artist', limit=20)
+        artists_list = artists['artists']['items']
+        if len(artists_list) > 0:
+            for artist in artists_list:
+                search_results.append(artist['name'])
+    elif search_selected == 'Song':
+        st.write("Start song search")
+        songs = sp.search(q='track:' + search_keyword, type='track', limit=20)
+        songs_list = songs['tracks']['items']
+        if len(songs_list) > 0:
+            for song in songs_list:
+                search_results.append(song['name'])
 
 selected_artist = None
 selected_track = None
 
 if search_selected == 'Artist':
     selected_artist = st.selectbox("Select your artist: ", search_results)
+elif search_selected == 'Song':
+    selected_track = st.selectbox("Select your song: ", search_results)
 
 if selected_artist is not None:
     artist_id = None
     artist_uri = None
-    
+
     for artist in artists_list:
         if selected_artist == artist['name']:
             artist_id = artist['id']
             artist_uri = artist['uri']
-    
+
     if artist_id is not None:
         artist_choice = ['Top Songs']
         selected_artist_choice = 'Top Songs'
-                
+
         if selected_artist_choice == 'Top Songs':
             artist_uri = 'spotify:artist:' + artist_id
             top_songs_result = sp.artist_top_tracks(artist_uri)
-            i=1
+            i = 1
             for track in top_songs_result['tracks']:
                 with st.container():
                     col1, col2, col3, col4 = st.columns((4, 4, 2, 2))
@@ -61,32 +74,34 @@ if selected_artist is not None:
                     col21, col22 = st.columns((11, 1))
                     col31, col32 = st.columns((11, 1))
                     col1.write(i)
-                    i=i+1
+                    i = i + 1
                     col2.write(track['name'])
-                    
+
                     with col3:
                         def feature_requested(track_id):
-                            track_features = sp.audio_features(track_id) 
+                            track_features = sp.audio_features(track_id)
                             df = pd.DataFrame(track_features, index=[0])
-                            df_features = df.loc[:, ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness', 'valence']]
-                            
+                            df_features = df.loc[:, ['acousticness', 'danceability', 'energy', 'instrumentalness',
+                                                     'liveness', 'speechiness', 'valence']]
+
                             with col21:
                                 st.dataframe(df_features)
                             with col31:
                                 polarplot.feature_plot(df_features)
-                            
+
                         feature_button_state = col3.button('Track Audio Features', key='features_' + track['id'])
                         if feature_button_state:
                             feature_requested(track['id'])
-                    
+
                     with col4:
                         def similar_songs_requested(track_id):
                             token = songrecommendations.get_token(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
                             similar_songs_json = songrecommendations.get_track_recommendations(track_id, token)
                             recommendation_list = similar_songs_json['tracks']
                             recommendation_list_df = pd.DataFrame(recommendation_list)
-                            recommendation_df = recommendation_list_df[['name', 'explicit', 'duration_ms', 'popularity']]
-                            
+                            recommendation_df = recommendation_list_df[
+                                ['name', 'explicit', 'duration_ms', 'popularity']]
+
                             with col21:
                                 st.dataframe(recommendation_df)
                             with col31:
@@ -95,5 +110,5 @@ if selected_artist is not None:
                         similar_songs_state = col4.button('Similar Songs', key='similar_songs_' + track['id'])
                         if similar_songs_state:
                             similar_songs_requested(track['id'])
-                    
+
                     st.write('----')
