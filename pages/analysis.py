705b82f5e8da62b681cdede61bc92c883bc63238
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-17 10:55:44 +0530

    update

--- pages/analysis.py
+++ pages/analysis.py
@@ -60,16 +60,16 @@ if selected_search_result:
     selected_id, selected_uri = get_id_uri(sp, search_type, selected_search_result)
 
 if selected_id:
-    if search_selected == 'Artist':
-        top_songs_result = sp.artist_top_tracks(selected_uri)
+    if selected_id and search_selected == 'Artist':
         for i, track in enumerate(top_songs_result['tracks'], start=1):
             st.write(f"{i}. {track['name']}")
             if st.button('Track Audio Features', key='features_' + track['id']):
                 display_features(track['id'])
             if st.button('Similar Songs', key='similar_' + track['id']):
                 display_similar_songs(track['id'])
-    elif search_selected == 'Song':
+
+    elif selected_id and search_selected == 'Song':
         if st.button('Track Audio Features', key='features_' + selected_id):
             display_features(selected_id)
         if st.button('Similar Songs', key='similar_' + selected_id):
-            display_similar_songs(selected_id)
+                display_similar_songs(selected_id)
