11d9a5c744379f3373e46a590012f7759515307c
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-17 14:22:06 +0530

    bg

--- pages/home.py
+++ pages/home.py
@@ -17,21 +17,6 @@ background-position: top left;
 background-repeat: no-repeat;
 background-attachment: local;
 }}
-
-[data-testid="stSidebar"] > div:first-child {{
-background-image: url("data:image/png;base64,{img}");
-background-position: center; 
-background-repeat: no-repeat;
-background-attachment: fixed;
-}}
-
-[data-testid="stHeader"] {{
-background: rgba(0,0,0,0);
-}}
-
-[data-testid="stToolbar"] {{
-right: 2rem;
-}}
 </style>
 """
 
