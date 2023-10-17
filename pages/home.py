7705dedacdc48d30a69e4f9047d2bfd12c8cd5fd
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-17 16:58:03 +0530

    image

--- pages/home.py
+++ pages/home.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="HOME", page_icon="🏠")
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
+background-image: url("https://images.unsplash.com/photo-1569982175971-d92b01cf8694?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2535&q=80");
 background-size: 180%;
 background-position: top left;
 background-repeat: no-repeat;
