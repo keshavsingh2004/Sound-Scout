31eb3b412c6e008cc5adbccdd3be77779a40569c
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-19 17:38:30 +0530

    rainbow

--- pages/home.py
+++ pages/home.py
@@ -95,7 +95,7 @@ genre_counts = pd.Series(genres_list).value_counts()
 top_5_genres = genre_counts.head(5)
 
 # Create a Streamlit app
-st.header("Home")
+st.markdown("# :rainbow[Home]")
 st.write("Welcome to [Your Website Name], where the power of music comes alive through analysis, exploration, and prediction.")
 st.header("Analysis of Songs")
 st.write("We offer a comprehensive analysis of songs, going beyond just the surface level. Our detailed analysis dives into various aspects of songs, including danceability, acousticness, and more, providing you with a deeper understanding of the music you love. Discover the essence of each track and find similar songs to expand your musical horizons.")
