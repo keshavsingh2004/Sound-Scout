0a740b29c3873d3abc363410a27c963c2dae2c06
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-19 18:24:47 +0530

    echo

--- pages/app.py
+++ pages/app.py
@@ -1,17 +1,16 @@
 from pathlib import Path
 import streamlit as st
-with st.echo("below"):
-    from st_pages import Page, Section, add_page_title, show_pages
+from st_pages import Page, Section, add_page_title, show_pages
 
     ## Declaring the pages in your app 📄:
 
-    show_pages(
-        [
-            Page("pages/home.py", "🏠 Home"),
-            Page("pages/analysis.py","🎶 Analysis of Songs"),
-            Page("pages/top_5.py", "🎤 Top Artists"),
-            Page("pages/analysisofgenre.py","🎧 Genre Analysis"),
-            Page("pages/linear.py","🔍 Genre Prediction"),  
-        ]
-    )
-    add_page_title()
+show_pages(
+    [
+        Page("pages/home.py", "🏠 Home"),
+        Page("pages/analysis.py","🎶 Analysis of Songs"),
+        Page("pages/top_5.py", "🎤 Top Artists"),
+        Page("pages/analysisofgenre.py","🎧 Genre Analysis"),
+        Page("pages/linear.py","🔍 Genre Prediction"),  
+    ]
+)
+add_page_title()
