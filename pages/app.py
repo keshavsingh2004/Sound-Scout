a4a6143f9b81df518a9f3ad420fa7ef76cd80da7
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-30 18:41:47 +0530

    sidebar

--- pages/app.py
+++ pages/app.py
@@ -3,6 +3,9 @@ import streamlit as st
 from st_pages import Page, Section, add_page_title, show_pages
 
     ## Declaring the pages in your app 📄:
+from st_lottie import st_lottie
+with st.sidebar:
+    st_lottie("https://lottie.host/4f9ac901-e5a7-4197-8e7a-c082c526cc21/4J94W1TV2f.json")
 
 show_pages(
     [
