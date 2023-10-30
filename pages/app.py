fc38b253ada929c31abd99d4671315bf0c099cee
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-30 18:51:01 +0530

    gif

--- pages/app.py
+++ pages/app.py
@@ -1,13 +1,14 @@
 from pathlib import Path
 import streamlit as st
 from st_pages import Page, Section, add_page_title, show_pages
-
-    ## Declaring the pages in your app 📄:
 from streamlit_lottie import st_lottie
+
+# Declaring the pages in your app 📄:
+with st.sidebar:
+    st_lottie("https://lottie.host/4f9ac901-e5a7-4197-8e7a-c082c526cc21/4J94W1TV2f.json")
 
 show_pages(
     [
-        st_lottie("https://lottie.host/4f9ac901-e5a7-4197-8e7a-c082c526cc21/4J94W1TV2f.json")
         Page("pages/home.py", "🏠 Home"),
         Page("pages/analysisofsongs.py","🎶 Analysis of Songs"),
         Page("pages/analysisofartists.py", "🎤 Analysis of Artists"),
