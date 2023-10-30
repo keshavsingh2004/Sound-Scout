d5b5b0f1dd80227ebd7cb7b854bdefee45458626
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-10-30 18:52:45 +0530

    page

--- pages/app.py
+++ pages/app.py
@@ -7,13 +7,13 @@ from streamlit_lottie import st_lottie
 with st.sidebar:
     st_lottie("https://lottie.host/4f9ac901-e5a7-4197-8e7a-c082c526cc21/4J94W1TV2f.json")
 
-show_pages(
-    [
-        Page("pages/home.py", "🏠 Home"),
-        Page("pages/analysisofsongs.py","🎶 Analysis of Songs"),
-        Page("pages/analysisofartists.py", "🎤 Analysis of Artists"),
-        Page("pages/analysisofgenre.py","🎧 Genre Analysis"),
-        Page("pages/genreprediction.py","🔍 Genre Prediction"),  
-    ]
-)
-add_page_title()
+    show_pages(
+        [
+            Page("pages/home.py", "🏠 Home"),
+            Page("pages/analysisofsongs.py","🎶 Analysis of Songs"),
+            Page("pages/analysisofartists.py", "🎤 Analysis of Artists"),
+            Page("pages/analysisofgenre.py","🎧 Genre Analysis"),
+            Page("pages/genreprediction.py","🔍 Genre Prediction"),  
+        ]
+    )
+    add_page_title()
