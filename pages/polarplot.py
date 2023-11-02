f18a1360fdc30f7710bc53dd4d5651d22a61c238
Swayam Prasad Sah <109921815+swayamps@users.noreply.github.com>
2023-11-03 00:29:47 +0530

    polarplot

--- pages/polarplot.py
+++ pages/polarplot.py
@@ -2,29 +2,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 
-def feature_plot(features):
+# def feature_plot(features):
 
+#     labels = features.columns.tolist()
+#     stats = features.mean().tolist()
+
+#     angles = np.linspace(0, 2*np.pi, len(labels), endpoint=False)
+
+#     # close the plot
+#     stats = np.concatenate((stats,[stats[0]]))
+#     angles = np.concatenate((angles,[angles[0]]))
+
+#     # Size of the figure
+#     fig = plt.figure(figsize=(18, 18))
+
+#     ax = fig.add_subplot(221, polar=True)
+#     ax.plot(angles, stats, 'o-', linewidth=2, label="Features", color='gray', alpha=0.75)
+#     ax.fill(angles, stats, alpha=0.5, facecolor='gray')
+#     ax.set_thetagrids(angles[0:7] * 180/np.pi, labels, fontsize=13)
+
+#     ax.set_rlabel_position(250)
+#     plt.yticks([0.2, 0.4, 0.6, 0.8], ["0.2", '0.4', "0.6", "0.8"], color="grey", size=12)
+#     plt.ylim(0, 1)
+
+#     plt.legend(loc='best', bbox_to_anchor=(0.1, 0.1))
+
+#     st.pyplot(fig, bbox_inches='tight')
+
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+def feature_plot(features):
     labels = features.columns.tolist()
     stats = features.mean().tolist()
+    stats = np.concatenate((stats, [stats[0]])
 
-    angles = np.linspace(0, 2*np.pi, len(labels), endpoint=False)
+    angles = np.linspace(0, 2 * np.pi, len(labels), endpoint=False)
+    angles = np.concatenate((angles, [angles[0]])
 
-    # close the plot
-    stats = np.concatenate((stats,[stats[0]]))
-    angles = np.concatenate((angles,[angles[0]]))
+    fig = px.line_polar(r=stats, theta=angles, line_close=True, title="Feature Radar Chart")
+    fig.update_polar(gridcolor='gray')
+    fig.update_layout(polar=dict(radialaxis=dict(ticksuffix='%'), angularaxis=dict(direction='clockwise'))
 
-    # Size of the figure
-    fig = plt.figure(figsize=(18, 18))
+    st.plotly_chart(fig)
 
-    ax = fig.add_subplot(221, polar=True)
-    ax.plot(angles, stats, 'o-', linewidth=2, label="Features", color='gray', alpha=0.75)
-    ax.fill(angles, stats, alpha=0.5, facecolor='gray')
-    ax.set_thetagrids(angles[0:7] * 180/np.pi, labels, fontsize=13)
-
-    ax.set_rlabel_position(250)
-    plt.yticks([0.2, 0.4, 0.6, 0.8], ["0.2", '0.4', "0.6", "0.8"], color="grey", size=12)
-    plt.ylim(0, 1)
-
-    plt.legend(loc='best', bbox_to_anchor=(0.1, 0.1))
-
-    st.pyplot(fig, bbox_inches='tight')
+# Example usage:
+# feature_plot(your_features_df)
