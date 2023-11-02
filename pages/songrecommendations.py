589bf13874507c098557b392ec7468f20ed02ad9
Swayam Prasad Sah <109921815+swayamps@users.noreply.github.com>
2023-11-03 00:11:51 +0530

    plotly

--- pages/songrecommendations.py
+++ pages/songrecommendations.py
@@ -2,6 +2,7 @@ import requests
 import base64
 
 import matplotlib.pyplot as plt
+import plotly.express as px
 import numpy as np
 import seaborn as sns
 import streamlit as st
@@ -33,27 +34,57 @@ def get_track_recommendations(seed_tracks,token):
     res = requests.get(url=recUrl, headers=headers)
     return res.json()
 
-def song_recommendation_vis(reco_df):    
-    reco_df['duration_min'] = round(reco_df['duration_ms'] / 1000, 0)
-    reco_df["popularity_range"] = reco_df["popularity"] - (reco_df['popularity'].min() - 1)
+# def song_recommendation_vis(reco_df):    
+#     reco_df['duration_min'] = round(reco_df['duration_ms'] / 1000, 0)
+#     reco_df["popularity_range"] = reco_df["popularity"] - (reco_df['popularity'].min() - 1)
     
-    plt.figure(figsize=(15, 6), facecolor=(.9, .9, .9))    
+#     plt.figure(figsize=(15, 6), facecolor=(.9, .9, .9))    
 
-    x = reco_df['name']
-    y = reco_df['duration_min']
-    s = reco_df['popularity_range']*20
+#     x = reco_df['name']
+#     y = reco_df['duration_min']
+#     s = reco_df['popularity_range']*20
         
-    color_labels = reco_df['explicit'].unique()
-    rgb_values = sns.color_palette("Set1", 8)
-    color_map = dict(zip(color_labels, rgb_values))
+#     color_labels = reco_df['explicit'].unique()
+#     rgb_values = sns.color_palette("Set1", 8)
+#     color_map = dict(zip(color_labels, rgb_values))
 
-    plt.scatter(x, y, s, alpha=0.7, c=reco_df['explicit'].map(color_map))
-    plt.xticks(rotation=90)
-    plt.legend()
-    # show the graph
-    plt.show()
+#     plt.scatter(x, y, s, alpha=0.7, c=reco_df['explicit'].map(color_map))
+#     plt.xticks(rotation=90)
+#     plt.legend()
+#     # show the graph
+#     plt.show()
     
-    st.pyplot(plt)
+#     st.pyplot(plt)
+
+def song_recommendation_vis(reco_df):
+    # Convert the duration to minutes
+    reco_df['duration_min'] = round(reco_df['duration_ms'] / 1000, 0)
+
+    # Calculate the popularity range
+    reco_df['popularity_range'] = reco_df['popularity'] - (reco_df['popularity'].min() - 1)
+
+    # Create a Plotly figure
+    fig = px.scatter(
+        x=reco_df['name'],
+        y=reco_df['duration_min'],
+        size=reco_df['popularity_range'] * 20,
+        color=reco_df['explicit'].unique(),
+        hover_name=reco_df['name'],
+    )
+
+    # Set the title and axis labels
+    fig.update_layout(
+        title='Song Recommendations',
+        xaxis_title='Song Name',
+        yaxis_title='Duration (min)',
+        legend_title='Explicit',
+    )
+
+    # Rotate the x-axis labels
+    fig.update_xaxes(tickangle=90)
+
+    # Show the plot in Streamlit
+    st.plotly_chart(fig)
     
 
 def save_album_image(img_url, track_id):
