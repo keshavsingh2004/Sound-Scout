0d7276845e356da4d475e5f3158e2a4b0d9fe687
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-11-03 01:54:46 +0530

    playlist

--- pages/app.py
+++ pages/app.py
@@ -7,10 +7,11 @@ from st_pages import Page, Section, add_page_title, show_pages
 show_pages(
         [
             Page("pages/home.py", "🏠 Home"),
-            Page("pages/analysisofsongs.py","🎶 Analysis of Songs"),
+            Page("pages/analysisofsongs.py","🎵 Analysis of Songs"),
             Page("pages/analysisofartists.py", "🎤 Analysis of Artists"),
             Page("pages/analysisofgenre.py","🎧 Genre Analysis"),
-            Page("pages/genreprediction.py","🔍 Genre Prediction"),  
+            Page("pages/genreprediction.py","🔍 Genre Prediction"),
+            Page("pages/playlistanalysis.py","🎶 Analysis of Playlists")  
         ]
     )
 add_page_title()
