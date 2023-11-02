75a745a3854b300bcc6874939aaeb33d997de8fb
Swayam Prasad Sah <109921815+swayamps@users.noreply.github.com>
2023-11-02 18:50:05 +0530

    test

--- pages/analysisofsongs.py
+++ pages/analysisofsongs.py
@@ -23,6 +23,7 @@ search_keyword = st.text_input(search_selected + " (Keyword Search)")
 button_clicked = st.button("Search")
 
 search_results = []
+
 if search_keyword is not None and len(str(search_keyword)) > 0:
     if search_selected == 'Artist':
         st.write("Start artist search")
@@ -42,6 +43,7 @@ if search_keyword is not None and len(str(search_keyword)) > 0:
                 search_results.append(song['name'])
 
 selected_search_result = st.selectbox("Select your " + search_selected.lower() + ": ", search_results)
+
 if selected_search_result is not None:
     if search_selected == 'Artist':
         artist_id = None
@@ -53,58 +55,90 @@ if selected_search_result is not None:
                 artist_uri = artist['uri']
         
         if artist_id is not None:
-            artist_uri = 'spotify:artist:' + artist_id
-            top_songs_result = sp.artist_top_tracks(artist_uri)
-            i = 1
-            for track in top_songs_result['tracks']:
-                with st.container():
-                    col1, col2, col3, col4 = st.columns((4, 4, 2, 2))
-                    col11, col12 = st.columns((10, 2))
-                    col21, col22 = st.columns((11, 1))
-                    col31, col32 = st.columns((11, 1))
-                    col1.write(i)
-                    i = i + 1
-                    col2.write(track['name'])
-                    
-                    with col3:
-                        def feature_requested(track_id):
-                            track_features = sp.audio_features(track_id) 
-                            df = pd.DataFrame(track_features, index=[0])
-                            df_features = df.loc[:, ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness', 'valence']]
-                            
-                            with col21:
-                                st.dataframe(df_features)
-                            with col31:
-                                polarplot.feature_plot(df_features)
+                artist_uri = 'spotify:artist:' + artist_id
+                top_songs_result = sp.artist_top_tracks(artist_uri)
+                i=1
+                for track in top_songs_result['tracks']:
+                    with st.container():
+                        col1, col2, col3, col4 = st.columns((4, 4, 2, 2))
+                        col11, col12 = st.columns((10, 2))
+                        col21, col22 = st.columns((11, 1))
+                        col31, col32 = st.columns((11, 1))
+                        col1.write(i)
+                        i=i+1
+                        col2.write(track['name'])
                         
-                        feature_button_state = col3.button('Track Audio Features', key='features_' + track['id'])
-                        if feature_button_state:
-                            feature_requested(track['id'])
-                    
-                    with col4:
-                        def similar_songs_requested(track_id):
-                            token = get_token(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
-                            similar_songs_json = get_track_recommendations(track_id, token)
-                            recommendation_list = similar_songs_json['tracks']
-                            recommendation_list_df = pd.DataFrame(recommendation_list)
-                            recommendation_df = recommendation_list_df[['name', 'duration_ms', 'popularity', 'explicit']]
-                            recommendation_df['similarity'] = np.random.rand(len(recommendation_df))  # Example similarity values, replace with actual values
+                        with col3:
+                            def feature_requested(track_id):
+                                track_features = sp.audio_features(track_id) 
+                                df = pd.DataFrame(track_features, index=[0])
+                                df_features = df.loc[:, ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness', 'valence']]
+                                
+                                with col21:
+                                    st.dataframe(df_features)
+                                with col31:
+                                    polarplot.feature_plot(df_features)
                             
-                            with col21:
-                                st.dataframe(recommendation_df)
-                            with col31:
-                                song_recommendation_vis(recommendation_df)
+                            feature_button_state = col3.button('Track Audio Features', key='features_' + track['id'])
+                            if feature_button_state:
+                                feature_requested(track['id'])
                         
-                        similar_button_state = col4.button('Similar Songs', key='similar_' + track['id'])
-                        if similar_button_state:
-                            similar_songs_requested(track['id'])
-                    track_id = track['id']
-                    spotify_url = f"https://open.spotify.com/embed/track/{track_id}"
-                    st.markdown(f"""
-                        <iframe style="border-radius:12px" src="{spotify_url}" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                        <br><br>
-                    """, unsafe_allow_html=True)
-                    st.divider()
+                        with col4:
+                            def similar_songs_requested(track_id):
+                                original_track_features = sp.audio_features(track_id)
+                                original_features = np.array([
+                                    original_track_features[0]['acousticness'],
+                                    original_track_features[0]['danceability'],
+                                    original_track_features[0]['energy'],
+                                    original_track_features[0]['instrumentalness'],
+                                    original_track_features[0]['liveness'],
+                                    original_track_features[0]['speechiness'],
+                                    original_track_features[0]['valence']
+                                ])
+                                token = songrecommendations.get_token(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
+                                similar_songs_json = songrecommendations.get_track_recommendations(track_id, token)
+                                recommendation_list = similar_songs_json['tracks']
+                                recommendation_list_df = pd.DataFrame(recommendation_list)
+                                recommendation_df = recommendation_list_df[['name','duration_ms', 'popularity','explicit']]
+                                name_list = recommendation_df['name'].tolist()
+                                simi=[]
+
+                                for recommendations in name_list:
+                                    s_song = sp.search(q='track:' + recommendations, type='track', limit=20)
+                                    selected_track_id = s_song['id']
+                                    selected_track_features = sp.audio_features(selected_track_id)
+                                    selected_features = np.array([
+                                        selected_track_features[0]['acousticness'],
+                                        selected_track_features[0]['danceability'],
+                                        selected_track_features[0]['energy'],
+                                        selected_track_features[0]['instrumentalness'],
+                                        selected_track_features[0]['liveness'],
+                                        selected_track_features[0]['speechiness'],
+                                        selected_track_features[0]['valence']
+                                    ])
+
+                                    distance = calculate_euclidean_distance(original_features, selected_features)
+                                    distance=1-distance
+                                    simi.append(distance)
+                                recommendation_df["similarity"]=distance
+
+
+                                
+                                with col21:
+                                    st.dataframe(recommendation_df)
+                                with col31:
+                                    songrecommendations.song_recommendation_vis(recommendation_df)
+                            
+                            similar_button_state = col4.button('Similar Songs', key='similar_' + track['id'])
+                            if similar_button_state:
+                                similar_songs_requested(track['id'])
+                        track_id=track['id']
+                        spotify_url = f"https://open.spotify.com/embed/track/{track_id}"
+                        st.markdown(f"""
+                            <iframe style="border-radius:12px" src="{spotify_url}" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                            <br><br>
+                        """, unsafe_allow_html=True)
+                        st.divider()
      
     elif search_selected == 'Song':
         track_id = None
--- pages/songrecommendations.py
+++ pages/songrecommendations.py
@@ -7,7 +7,7 @@ import seaborn as sns
 import streamlit as st
 from PIL import Image
 
-def get_token(clientId, clientSecret):
+def get_token(clientId,clientSecret):
     url = "https://accounts.spotify.com/api/token"
     headers = {}
     data = {}
@@ -22,7 +22,7 @@ def get_token(clientId, clientSecret):
     return token
 
 
-def get_track_recommendations(seed_tracks, token):
+def get_track_recommendations(seed_tracks,token):
     limit = 10
     recUrl = f"https://api.spotify.com/v1/recommendations?limit={limit}&seed_tracks={seed_tracks}"
 
@@ -36,14 +36,13 @@ def get_track_recommendations(seed_tracks, token):
 def song_recommendation_vis(reco_df):    
     reco_df['duration_min'] = round(reco_df['duration_ms'] / 1000, 0)
     reco_df["popularity_range"] = reco_df["popularity"] - (reco_df['popularity'].min() - 1)
-    reco_df["similarity"] = np.random.rand(len(reco_df))  # Example similarity values, replace with actual values
     
     plt.figure(figsize=(15, 6), facecolor=(.9, .9, .9))    
 
     x = reco_df['name']
-    y = reco_df['similarity']
-    s = reco_df['popularity_range'] * 20
-
+    y = reco_df['duration_min']
+    s = reco_df['popularity_range']*20
+        
     color_labels = reco_df['explicit'].unique()
     rgb_values = sns.color_palette("Set1", 8)
     color_map = dict(zip(color_labels, rgb_values))
@@ -51,15 +50,18 @@ def song_recommendation_vis(reco_df):
     plt.scatter(x, y, s, alpha=0.7, c=reco_df['explicit'].map(color_map))
     plt.xticks(rotation=90)
     plt.legend()
-    
     # show the graph
     plt.show()
     
     st.pyplot(plt)
+    
 
 def save_album_image(img_url, track_id):
     r = requests.get(img_url)
     open('img/' + track_id + '.jpg', "wb").write(r.content)
     
-def get_album_image(track_id):
+def get_album_mage(track_id):
     return Image.open('img/' + track_id + '.jpg')
+
+def calculate_euclidean_distance(original_features, selected_features):
+    return np.linalg.norm(original_features - selected_features)
