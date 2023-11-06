3585fdd57fefe8fd1e65ba631d1facf907c943cc
Swayam Prasad Sah <109921815+swayamps@users.noreply.github.com>
2023-11-06 15:04:41 +0530

    ok

--- pages/analysisofsongs.py
+++ pages/analysisofsongs.py
@@ -209,14 +209,11 @@ if selected_search_result is not None:
                     simi=[]
 
                     for recommendations in name_list:
+                        selected_track_id = None
                         s_song = sp.search(q='track:' + recommendations, type='track', limit=20)
 
                         if 'tracks' in s_song and 'items' in s_song['tracks'] and len(s_song['tracks']['items']) > 0:
                             selected_track_id = s_song['tracks']['items'][0]['id']
-
-                        else:
-                            st.write("Please Wait...")
-                        if selected_track_id is not None:
                             selected_track_features = sp.audio_features(selected_track_id)
                         selected_features = np.array([
                             selected_track_features[0]['acousticness'],
