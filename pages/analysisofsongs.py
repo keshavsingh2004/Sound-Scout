093e84f45fa06631633e6e2ee1dd4af60a452a94
Swayam Prasad Sah <109921815+swayamps@users.noreply.github.com>
2023-11-08 19:51:40 +0530

    1

--- pages/analysisofsongs.py
+++ pages/analysisofsongs.py
@@ -29,8 +29,8 @@ if search_keyword is not None and len(str(search_keyword)) > 0:
     if search_selected == 'Artist':
         st.write("Start artist search")
         if search_keyword=="Arijit" or search_keyword=="Arijit Singh" or search_keyword=="arijit" or search_keyword=="arijit singh":
-            artists = sp.search(q='artist:' + search_keyword, type='artist', limit=20)
-            artists_list = artists['artists']['items'][0]
+            artists = sp.search(q='artist:' + search_keyword, type='artist', limit=1)
+            artists_list = artists['artists']['items']
         else:
             artists = sp.search(q='artist:' + search_keyword, type='artist', limit=20)
             artists_list = artists['artists']['items']
