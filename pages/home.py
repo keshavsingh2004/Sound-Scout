562e62ef59ac52b9bd77eb7f7f3453ec7d0c725a
Swayam Prasad Sah <109921815+swayamps@users.noreply.github.com>
2023-11-22 19:48:49 +0530

    home

--- pages/home.py
+++ pages/home.py
@@ -10,9 +10,9 @@ st.set_page_config(page_title="Home", page_icon="🏠")
 
 with open("designing.css") as source_des:
     st.markdown(f'<style>{source_des.read()}</style>', unsafe_allow_html=True)
-col1, col2, col3, col4 = st.columns([2, 4, 1, 1])
-with col1:
-    icon("Sound_Scout.png")
-with col2:
-    st.title("SOUND SCOUT")
+icon("Sound_Scout.png")
+# with col2:
+#     st.title("SOUND SCOUT")
 st.caption("A In-Depth Analysis of Everything you like about Music")
+st.subheader("What We Offer")
+col1, col2, col3, col4, col5 = st.columns(5)
