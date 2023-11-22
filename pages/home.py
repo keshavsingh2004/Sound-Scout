f77e8fc28a01bcf683d8d9665998392dc29c1fb9
Swayam Prasad Sah <109921815+swayamps@users.noreply.github.com>
2023-11-22 20:22:08 +0530

    buttons

--- pages/home.py
+++ pages/home.py
@@ -7,6 +7,23 @@ def icon(image_path: str):
     st.image(image_path, width=175)
 
 st.set_page_config(page_title="Home", page_icon="🏠")
+def redirect_button(url: str, text: str= None, color="#FD504D"):
+    st.markdown(
+    f"""
+    <a href="{url}" target="_self">
+        <div style="
+            display: inline-block;
+            padding: 0.5em 1em;
+            color: #FFFFFF;
+            background-color: {color};
+            border-radius: 3px;
+            text-decoration: none;">
+            {text}
+        </div>
+    </a>
+    """,
+    unsafe_allow_html=True
+    )
 
 with open("designing.css") as source_des:
     st.markdown(f'<style>{source_des.read()}</style>', unsafe_allow_html=True)
@@ -17,52 +34,12 @@ st.caption("A In-Depth Analysis of Everything you like about Music")
 st.header("What We Offer")
 col1, col2, col3, col4, col5 = st.columns(5)
 with col1:
-    st.markdown('<a href="https://sound-scout.streamlit.app/%F0%9F%8E%B5%20Analysis%20of%20Songs" target="_self"><div style="
-            display: inline-block;
-            padding: 0.5em 1em;
-            color: #FFFFFF;
-            background-color: {color};
-            border-radius: 3px;
-            text-decoration: none;">
-            {text}
-        </div>Analysis of Songs</a>', unsafe_allow_html=True)
-with col2:
-    st.markdown('<a href="https://sound-scout.streamlit.app/%F0%9F%8E%A4%20Analysis%20of%20Artists" target="_self"><div style="
-            display: inline-block;
-            padding: 0.5em 1em;
-            color: #FFFFFF;
-            background-color: {color};
-            border-radius: 3px;
-            text-decoration: none;">
-            {text}
-        </div>Analysis of Artists</a>', unsafe_allow_html=True)
-with col3:
-    st.markdown('<a href="https://sound-scout.streamlit.app/%F0%9F%8E%A7%20Genre%20Analysis" target="_self"><div style="
-            display: inline-block;
-            padding: 0.5em 1em;
-            color: #FFFFFF;
-            background-color: {color};
-            border-radius: 3px;
-            text-decoration: none;">
-            {text}
-        </div>Analysis of Genre</a>', unsafe_allow_html=True)
-with col4:
-    st.markdown('<a href="https://sound-scout.streamlit.app/%F0%9F%94%8D%20Genre%20Prediction" target="_self"><div style="
-            display: inline-block;
-            padding: 0.5em 1em;
-            color: #FFFFFF;
-            background-color: {color};
-            border-radius: 3px;
-            text-decoration: none;">
-            {text}
-        </div>Genre Prediction</a>', unsafe_allow_html=True)
-with col5:
-    st.markdown('<a href="https://sound-scout.streamlit.app/%F0%9F%8E%B6%20Analysis%20of%20Playlists" target="_self"><div style="
-            display: inline-block;
-            padding: 0.5em 1em;
-            color: #FFFFFF;
-            background-color: {color};
-            border-radius: 3px;
-            text-decoration: none;">
-            {text}
-        </div>Analysis of Playlists</a>', unsafe_allow_html=True)
+    redirect_button("https://sound-scout.streamlit.app/%F0%9F%8E%B5%20Analysis%20of%20Songs","Analysis of Songs")
+# with col2:
+
+# with col3:
+    
+# with col4:
+    
+# with col5:
+    
