2f1f8cf0760d12b461d0dd747de9357ecad8d491
Swayam Prasad Sah <109921815+swayamps@users.noreply.github.com>
2023-11-22 20:24:44 +0530

    buttons

--- pages/home.py
+++ pages/home.py
@@ -35,11 +35,11 @@ st.header("What We Offer")
 col1, col2, col3, col4, col5 = st.columns(5)
 with col1:
     redirect_button("https://sound-scout.streamlit.app/%F0%9F%8E%B5%20Analysis%20of%20Songs","Analysis of Songs")
-# with col2:
-
-# with col3:
-    
-# with col4:
-    
-# with col5:
-    
+with col2:
+    redirect_button("https://sound-scout.streamlit.app/%F0%9F%8E%A4%20Analysis%20of%20Artists","Analysis of Artists")
+with col3:
+    redirect_button("https://sound-scout.streamlit.app/%F0%9F%8E%A7%20Genre%20Analysis","Genre Analysis")
+with col4:
+    redirect_button("https://sound-scout.streamlit.app/%F0%9F%94%8D%20Genre%20Prediction","Genre Prediction")
+with col5:
+    redirect_button("https://sound-scout.streamlit.app/%F0%9F%8E%B6%20Analysis%20of%20Playlists","Analysis of Playlists")
