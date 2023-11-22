966d74bbfbd7194dae71aed2ab80df44e6348b82
Swayam Prasad Sah <109921815+swayamps@users.noreply.github.com>
2023-11-22 20:51:31 +0530

    collapsed

--- pages/home.py
+++ pages/home.py
@@ -6,7 +6,7 @@ def icon(image_path: str):
     """Shows an image as a Notion-style page icon."""
     st.image(image_path, width=175)
 
-st.set_page_config(page_title="Home", page_icon="🏠")
+st.set_page_config(page_title="Home", page_icon="🏠",initial_sidebar_state="collapsed")
 def redirect_button(url: str, text: str = None, color="rgba(0, 0, 0, 0.5)"):
     st.markdown(
         f"""
@@ -28,8 +28,6 @@ def redirect_button(url: str, text: str = None, color="rgba(0, 0, 0, 0.5)"):
 with open("designing.css") as source_des:
     st.markdown(f'<style>{source_des.read()}</style>', unsafe_allow_html=True)
 icon("Sound_Scout.png")
-# with col2:
-#     st.title("SOUND SCOUT")
 st.caption("In-Depth Analysis of Everything you like about Music")
 st.header("What We Offer")
 col1, col2, col3, col4, col5 = st.columns(5)
