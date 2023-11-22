49079cbacb5fd5960b1f8a2975c917647c5e8641
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-11-23 02:19:34 +0530

    option

--- pages/home.py
+++ pages/home.py
@@ -2,6 +2,7 @@ import streamlit as st
 from st_pages import add_page_title
 import pandas as pd
 from streamlit_extras.switch_page_button import switch_page 
+from streamlit_option_menu import option_menu
 
 # def icon(image_path: str):
 #     """Shows an image as a Notion-style page icon."""
@@ -32,3 +33,25 @@ with col5:
         switch_page("🎶 Analysis of Playlists")
 st.write(" ")
 st.info("Our platform offers a multifaceted exploration of the music industry, delving into both artist-specific and genre-wide analyses. For artists, we provide an extensive discography and a tool for insightful comparisons, enabling enthusiasts and professionals alike to trace the trajectory of their favorite musicians. Moving to genres, we don't just scratch the surface but conduct a thorough examination of their evolution, offering a deep dive into the historical shifts and trends that have shaped musical landscapes. For those curious about the future, our genre prediction feature employs machine learning algorithms, allowing users to visualize and compare predictions against actual data. Our playlist analysis goes beyond the surface, dissecting the intricate features of each song to uncover trends in tempo, mood, and instrumentation. Dive deep into the musical DNA of your playlists, gaining valuable insights into the sonic landscapes that resonate with your preferences. Our platform is a comprehensive guide for anyone seeking a nuanced understanding of the dynamic world of music.")
+selected = option_menu(
+    menu_title=None,
+    options=["🏠 Home", "🎵 Analysis of Songs", "🎤 Analysis of Artists", "🎧 Analysis of Genre", "🔍 Genre Prediction", "🎶 Analysis of Playlists"],
+    icons=["house", "book", "envelope", "house", "book", "envelope"],
+    menu_icon="cast",
+    default_index=0,
+    orientation="horizontal",
+)
+
+if selected == "🏠 Home":
+    switch_page("🏠 Home")
+elif selected == "🎵 Analysis of Songs":
+    switch_page("🎵 Analysis of Songs")
+elif selected == "🎤 Analysis of Artists":
+    switch_page("🎤 Analysis of Artists")
+elif selected == "🎧 Analysis of Genre":
+    switch_page("🎧 Analysis of Genre")
+elif selected == "🔍 Genre Prediction":
+    switch_page("🔍 Genre Prediction")
+elif selected == "🎶 Analysis of Playlists":
+    switch_page("🎶 Analysis of Playlists")
+
