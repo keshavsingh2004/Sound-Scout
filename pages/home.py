846ac27ca38268ef194ddcba49cec2833b1b5210
Swayam Prasad Sah <109921815+swayamps@users.noreply.github.com>
2023-11-22 21:07:50 +0530

    hover

--- pages/home.py
+++ pages/home.py
@@ -10,25 +10,34 @@ st.set_page_config(page_title="Home", page_icon="🏠",initial_sidebar_state="co
 def redirect_button(url: str, text: str = None, color="rgba(0, 0, 0, 0.5)"):
     st.markdown(
         f"""
+        <style>
+        .button-container {{
+            display: inline-block;
+            padding: 0.5em 1em;
+            color: #FFFFFF;
+            background-color: {color};
+            border-radius: 10px;
+            text-decoration: none;
+            transition: background-color 0.3s ease;
+        }}
+        
+        .button-container:hover {{
+            background-color: rgba(0, 0, 0, 0.8);
+        }}
+        </style>
+        
         <a href="{url}" target="_self">
-            <div style="
-                display: inline-block;
-                padding: 0.5em 1em;
-                color: #FFFFFF;
-                background-color: {color};
-                border-radius: 10px;
-                text-decoration: none;">
+            <div class="button-container">
                 {text}
             </div>
         </a>
         """,
         unsafe_allow_html=True
     )
-
 with open("designing.css") as source_des:
     st.markdown(f'<style>{source_des.read()}</style>', unsafe_allow_html=True)
 icon("Sound_Scout.png")
-st.caption("In-Depth Analysis of Everything you like about Music")
+st.caption("In-Depth Analysis of everything you like about Music")
 st.header("What We Offer")
 col1, col2, col3, col4, col5 = st.columns(5)
 with col1:
