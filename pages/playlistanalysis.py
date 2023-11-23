09383963b16fdf0b8ddf723295819978fdea3189
Swayam Prasad Sah <109921815+swayamps@users.noreply.github.com>
2023-11-24 00:50:05 +0530

    fixing errors

--- pages/playlistanalysis.py
+++ pages/playlistanalysis.py
@@ -27,6 +27,24 @@ st.title('Spotify Playlist Analysis')
 url = st.text_input('Enter the Spotify playlist link or playlist ID')
 parsed_url = urllib.parse.urlparse(url)
 playlist_id = parsed_url.path.split('/')[-1]
+col1, col2, col3,col4= st.columns(3)
+st.markdown("""
+    <style>
+    div.stButton > button:first-child  {
+    position: fixed;
+    bottom: 10px;
+}
+</style>
+    """, unsafe_allow_html=True)
+with col1:
+    pass
+with col3:
+    pass
+with col2:
+    pass
+with col4:
+    if st.button('Take me Home 🏠'):
+       switch_page("🏠 Home")
 
 #playlist_id = '561Z6T9i38xWLoPQIMIbBs'
 try:
@@ -271,19 +289,3 @@ except spotipy.exceptions.SpotifyException as e:
                 st.error("Playlist not found. Please check the playlist link or ID and try again.")
             else:
                 st.error(f"An error occurred: {e}. Please try again with a different playlist ID.")
-col1, col2, col3= st.columns(3)
-st.markdown("""
-    <style>
-    div.stButton > button:first-child  {
-    position: fixed;
-    bottom: 10px;
-}
-</style>
-    """, unsafe_allow_html=True)
-with col1:
-    pass
-with col3:
-    pass
-with col2:
-    if st.button('Take me Home 🏠'):
-       switch_page("🏠 Home")
