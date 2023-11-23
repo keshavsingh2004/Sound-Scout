3f914bc4f4cc79f37f22e4e04c0676502798d9c2
Keshav Singh <115015479+keshavsingh2004@users.noreply.github.com>
2023-11-23 23:32:00 +0530

    try

--- pages/home.py
+++ pages/home.py
@@ -3,6 +3,7 @@ from st_pages import add_page_title
 import pandas as pd
 from streamlit_extras.switch_page_button import switch_page 
 from streamlit_option_menu import option_menu
+import hydralit as hy
 
 st.set_page_config(page_title="Home", page_icon="🏠",initial_sidebar_state="collapsed")
 with open("designing.css") as source_des:
@@ -29,3 +30,18 @@ with col5:
         switch_page("🎶 Analysis of Playlists")
 st.write(" ")
 st.info("Our platform offers a multifaceted exploration of the music industry, delving into both artist-specific and genre-wide analyses. For artists, we provide an extensive discography and a tool for insightful comparisons, enabling enthusiasts and professionals alike to trace the trajectory of their favorite musicians. Moving to genres, we don't just scratch the surface but conduct a thorough examination of their evolution, offering a deep dive into the historical shifts and trends that have shaped musical landscapes. For those curious about the future, our genre prediction feature employs machine learning algorithms, allowing users to visualize and compare predictions against actual data. Our playlist analysis goes beyond the surface, dissecting the intricate features of each song to uncover trends in tempo, mood, and instrumentation. Dive deep into the musical DNA of your playlists, gaining valuable insights into the sonic landscapes that resonate with your preferences. Our platform is a comprehensive guide for anyone seeking a nuanced understanding of the dynamic world of music.")
+#when we import hydralit, we automatically get all of Streamlit
+
+app = hy.HydraApp(title='Simple Multi-Page App')
+
+@app.addapp()
+def my_home():
+    switch_page("🎵 Analysis of Songs")
+
+@app.addapp()
+def app2():
+ hy.info('Hello from app 2')
+
+
+#Run the whole lot, we get navbar, state management and app isolation, all with this tiny amount of work.
+app.run()
