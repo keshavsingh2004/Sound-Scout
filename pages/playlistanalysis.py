023db1aa9ce125f02aa6f36ad3f297f822b4903e
Swayam Prasad Sah <109921815+swayamps@users.noreply.github.com>
2023-11-24 00:55:17 +0530

    push

--- pages/playlistanalysis.py
+++ pages/playlistanalysis.py
@@ -27,22 +27,22 @@ st.title('Spotify Playlist Analysis')
 url = st.text_input('Enter the Spotify playlist link or playlist ID')
 parsed_url = urllib.parse.urlparse(url)
 playlist_id = parsed_url.path.split('/')[-1]
-col1, col2, col3,col4= st.columns(4)
-st.markdown("""
-    <style>
-    div.stButton > button:first-child  {
-    position: fixed;
-    bottom: 10px;
-}
-</style>
-    """, unsafe_allow_html=True)
+col1, col2, col3= st.columns(3)
+# st.markdown("""
+#     <style>
+#     div.stButton > button:first-child  {
+#     position: fixed;
+#     bottom: 10px;
+# }
+# </style>
+#     """, unsafe_allow_html=True)
 with col1:
     pass
 with col3:
     pass
 with col2:
-    pass
-with col4:
+    for _ in range(20):
+        st.write(" ")
     if st.button('Take me Home 🏠'):
        switch_page("🏠 Home")
 
@@ -284,6 +284,15 @@ try:
             # Display the insights for the cluster
             st.markdown(f"## Insights for {cluster_label}:")
             st.markdown(f"<p>{insights}</p>", unsafe_allow_html=True)
+            col1, col2, col3= st.columns(3)
+            with col1:
+                pass
+            with col3:
+                pass
+            with col2:
+                if st.button('Take me Home 🏠'):
+                    switch_page("🏠 Home")
+
 except spotipy.exceptions.SpotifyException as e:
             if e.http_status == 404:
                 st.error("Playlist not found. Please check the playlist link or ID and try again.")
