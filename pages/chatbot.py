3e3e9a24f91b830c598b6ef283660feb50988c78
Swayam Prasad Sah <109921815+swayamps@users.noreply.github.com>
2023-11-27 21:12:45 +0530

    prompt

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -149,7 +149,7 @@ def chatbot(df, selected_song_details):
 
     if follow_up_question:
         # Include the follow-up question in the prompt
-        prompt += f"{follow_up_question}\n\n"
+        prompt += f"Give me a paragraph on the query: {follow_up_question}\n\n"
 
         # Generate response using AI21
         response = ai21.Completion.execute(
