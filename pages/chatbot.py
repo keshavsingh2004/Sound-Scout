8a82a1c0025ae45381d562e59d3b3efd85c3bd06
Swayam Prasad Sah <109921815+swayamps@users.noreply.github.com>
2023-11-27 19:21:05 +0530

    fixing errors

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -78,10 +78,12 @@ def get_playlist_data(playlist_id):
             audio_features_df = audio_features_df[['id', 'danceability', 'energy', 'key', 'loudness', 'mode',
                                             'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence',
                                             'tempo', 'duration_ms', 'time_signature']]
+            merged_df = pd.merge(data, audio_features_df, on='id')
         except:
             audio_features_df = None
+            merged_df=data
         # Merge track information with audio features
-        merged_df = pd.merge(data, audio_features_df, on='id')
+        
 
         return merged_df
     except spotipy.exceptions.SpotifyException as e:
